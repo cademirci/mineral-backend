04bb20b1fcb9959551c930aa1b329fb4da05d0c7
UtkanKokturk <utkankokturk2001@gmail.com>
2023-12-17 15:26:17 +0300

    OpenAi request and response has been made

--- main.py
+++ main.py
@@ -1,40 +1,46 @@
-from bardapi import Bard
-import os
+import openai
 
-os.environ["_BARD_API_KEY"] ="dwgdqmk_nqZKrSOThDpnvwNeaCSbFU5AToVBJpVv8KOqzaSwgrUM7vFgORWH5MpW_0SwQw."
+# Set your OpenAI API key
+openai.api_key = 'YOUR_API_KEY'
 
-def bard_question_answer(question):
-    bard = Bard()
-    answer = bard.get_answer(question)
-    return answer["content"]
+def openai_question_answer(question):
+    response = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo-1106",
+        messages=[
+            {"role": "system", "content": "You are a helpful assistant."},
+            {"role": "user", "content": question}
+        ],
+        temperature=0.7,
+        max_tokens=30,
+        n=1
+    )
+    return response['choices'][0]['message']['content'].strip()
 
 if __name__ == "__main__":
-    mineral_name = input("Enter Mineral Name:")
-    
+    mineral_name = input("Enter Mineral Name: ")
+
     while True:
-        type_selection = input(f"Please choose what you want to write for {mineral_name} (0 for poetry, 1 for story): ")  
+        type_selection = input(f"Please choose what you want to write for {mineral_name} (0 for poetry, 1 for story): ")
 
         if type_selection == "0":
-            type= "poem"
+            type = "poem"
             length = input(f"{type.capitalize()} Determine the number of quatrains: ")
             type1 = input(f"{type.capitalize()} Determine the genre: ")
-            lang = input(f"{type.capitalize()} Determine your language: ") 
-            question =f"Can you write a {length} stanza {type} in {lang}   about the {mineral_name} mineral,the subject of which is {type1}?"
-            answer = bard_question_answer(question)
+            lang = input(f"{type.capitalize()} Determine your language: ")
+            question = f"Can you write a {length} stanza {type} in {lang} about the {mineral_name} mineral, the subject of which is {type1}?"
+            answer = openai_question_answer(question)
             break
         elif type_selection == "1":
-            type = "story"            
+            type = "story"
             length = input(f"{type.capitalize()} Specify the number of paragraphs:")
             type1 = input(f"{type.capitalize()} State the subject:")
-            lang = input(f"{type.capitalize()} Please specify your language:") 
+            lang = input(f"{type.capitalize()} Please specify your language:")
             question = f"Can you write a {type} about the {mineral_name} mineral,{length} paragraph long, with a {type1} subject, in {lang}?"
-            answer = bard_question_answer(question)
+            answer = openai_question_answer(question)
             break
         else:
-         print("Invalid selection,Select again.")
-         continue
+            print("Invalid selection, Select again.")
+            continue
 
-
-    print("\nBard API's Answer:")
+    print("\nOpenAI API's Answer:")
     print(answer)
-
